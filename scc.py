a267c94f153dd23505b14ffa85fb78265cb15cbd
Fellipe293 <Fellipe_293@hotmail.com>
2016-04-18 19:11:08 -0300

    Update scc.py

--- scc.py
+++ scc.py
@@ -1,1 +1,10 @@
+G = { }
+for i in range(1, 875715) :
+    G[i] = []
 
+for l in open('SCC.txt','r').readlines():
+    ins = []
+    ins = l.split()
+    x = int(ins[0])
+    y = int(ins[1])
+    G[x].append(y)
